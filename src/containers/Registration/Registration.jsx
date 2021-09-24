c060775d12c9bdbeb818cb8d878f41c9702125a2
Evgen <kurakin.evgeny@gmail.com>
2021-09-24 13:16:12 +0300

    Добавил компонент Registration

--- src/containers/Registration/Registration.jsx
+++ src/containers/Registration/Registration.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import classes from './Registration.css'
+import Button from '../../components/UI/Button/Button'
 
 const Registration = () => {
+  const handleClick = () => {
+    console.log('Registration Click')
+  }
   return (
     <section className={classes.registration}>
       <div className={classes.formGroup}>
+        <h1>Регистрация</h1>
         <input
           type="text"
           placeholder="Login"
@@ -15,6 +20,14 @@ const Registration = () => {
           placeholder="Password"
           className={classes.formControl}
         />
+        <div className={classes.formButton}>
+          <Button type={'registration'} onClick={() => handleClick()}>
+            Регистрация
+          </Button>
+          <div className={classes.formButtonLink}>
+            <a href="#"> Зарегистрироваться</a>
+          </div>
+        </div>
       </div>
     </section>
   )
