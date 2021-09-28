90930a10e5748d4890311f83adcd66c0b2065a22
Evgen <kurakin.evgeny@gmail.com>
2021-09-28 11:57:28 +0300

    Рефакторинг Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,12 @@ import logoImg from '../../assets/img/logo.png'
 const Header = () => {
   return (
     <div className={classes.header}>
-      <div className={classes.logo}>
-        <img src={logoImg} alt="" />
-        <div>
-          <Link to="/">S U M M E R &nbsp;&nbsp;H O T E L</Link>
+      <Link to="/">
+        <div className={classes.logo}>
+          <img src={logoImg} alt="" />
+          <div>S U M M E R &nbsp;&nbsp;H O T E L</div>
         </div>
-      </div>
+      </Link>
       <div className={classes.nav}>
         <Link to="/login">Войти</Link>
         <span>&nbsp;/&nbsp;</span>
