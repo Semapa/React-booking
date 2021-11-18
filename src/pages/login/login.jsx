9bb2bdd5becf21a70cb24e4be71d08e3eb54976b
Evgen <kurakin.evgeny@gmail.com>
2021-11-18 13:39:29 +0300

    Добвил хук useForm, сделал форму регистрации

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,61 +1,42 @@
-import { React, useState, useEffect } from 'react'
+import React from 'react'
 import classes from './login.css'
 import Button from '../../components/common/button/button'
 import { Link } from 'react-router-dom'
 import TextField from '../../components/common/form/textField/textField'
-import { validator } from '../../utils/validator'
 
-const Login = () => {
-  const [data, setData] = useState({ email: '', password: '' })
-  const [errors, setErrors] = useState({})
+import useForm from '../../hooks/useForm'
 
-  const validatorConfig = {
-    email: {
-      isRequired: {
-        message: 'Электронная почта обязательна для заполнения'
-      },
-      isEmail: {
-        message: 'Email введен не корректно'
-      }
+const validatorConfig = {
+  email: {
+    isRequired: {
+      message: 'Электронная почта обязательна для заполнения'
     },
-    password: {
-      isRequired: {
-        message: 'Пароль обязателен для заполнения'
-      },
-      isCapitalSymbol: {
-        message: 'Пароль должен содержать хотя бы одну заглавную букву'
-      },
-      isContainDigit: {
-        message: 'Пароль должен содержать хотя бы одну цифру'
-      },
-      min: {
-        message: 'Пароль должен состоять минимум из 8 символов',
-        value: 8
-      }
+    isEmail: {
+      message: 'Email введен не корректно'
+    }
+  },
+  password: {
+    isRequired: {
+      message: 'Пароль обязателен для заполнения'
+    },
+    isCapitalSymbol: {
+      message: 'Пароль должен содержать хотя бы одну заглавную букву'
+    },
+    isContainDigit: {
+      message: 'Пароль должен содержать хотя бы одну цифру'
+    },
+    min: {
+      message: 'Пароль должен состоять минимум из 8 символов',
+      value: 8
     }
   }
+}
 
-  useEffect(() => {
-    validate()
-  }, [data])
-
-  const validate = () => {
-    const errors = validator(data, validatorConfig)
-    setErrors(errors)
-    return Object.keys(errors).length === 0
-  }
-  const isValid = Object.keys(errors).length === 0
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const isValid = validate()
-    if (!isValid) return
-    console.log('data', data)
-  }
-
-  const handleChange = ({ target }) => {
-    setData((prevState) => ({ ...prevState, [target.name]: target.value }))
-  }
+const Login = () => {
+  const { form, handleSubmit, handleChange, errors, isValid } = useForm(
+    { email: '', password: '' },
+    validatorConfig
+  )
 
   return (
     <section className={classes.login}>
@@ -64,7 +45,7 @@ const Login = () => {
         <TextField
           placeholder="Email"
           name="email"
-          value={data.email}
+          value={form.email}
           onChange={handleChange}
           error={errors.email}
         />
@@ -72,7 +53,7 @@ const Login = () => {
           placeholder="Password"
           type="password"
           name="password"
-          value={data.password}
+          value={form.password}
           onChange={handleChange}
           error={errors.password}
         />
--- src/pages/registration/registration.jsx
+++ src/pages/registration/registration.jsx
@@ -1,31 +1,72 @@
 import React from 'react'
 import classes from './registration.css'
+import { Link } from 'react-router-dom'
 import Button from '../../components/common/button/button'
+import TextField from '../../components/common/form/textField/textField'
+
+import useForm from '../../hooks/useForm'
+
+const validatorConfig = {
+  email: {
+    isRequired: {
+      message: 'Электронная почта обязательна для заполнения'
+    },
+    isEmail: {
+      message: 'Email введен не корректно'
+    }
+  },
+  password: {
+    isRequired: {
+      message: 'Пароль обязателен для заполнения'
+    },
+    isCapitalSymbol: {
+      message: 'Пароль должен содержать хотя бы одну заглавную букву'
+    },
+    isContainDigit: {
+      message: 'Пароль должен содержать хотя бы одну цифру'
+    },
+    min: {
+      message: 'Пароль должен состоять минимум из 8 символов',
+      value: 8
+    }
+  }
+}
 
 const Registration = () => {
-  const handleClick = () => {
-    console.log('Registration Click')
-  }
+  const { form, handleSubmit, handleChange, errors, isValid } = useForm(
+    { email: '', password: '' },
+    validatorConfig
+  )
+
   return (
     <section className={classes.registration}>
-      <div className={classes.formGroup}>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <h1>Регистрация</h1>
-        <input
-          type="text"
-          placeholder="Login"
-          className={classes.formControl}
+        <TextField
+          placeholder="Email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          error={errors.email}
         />
-        <input
-          type="text"
+        <TextField
           placeholder="Password"
-          className={classes.formControl}
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          error={errors.password}
         />
-        <div className={classes.formButton}>
-          <Button typeButton={'primary'} onClick={() => handleClick()}>
-            Регистрация
+
+        <div className={classes.button}>
+          <Button typeButton={'primary'} type="submit" disabled={!isValid}>
+            Войти
           </Button>
+          <div className={classes.link}>
+            <Link to="/login">Войти</Link>
+          </div>
         </div>
-      </div>
+      </form>
     </section>
   )
 }
