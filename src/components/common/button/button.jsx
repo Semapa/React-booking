c8256522d058b59fef8888339866224d95ee8c0d
Evgen <kurakin.evgeny@gmail.com>
2021-11-18 11:33:33 +0300

    Пофиксил баг в компоненте Button не было пропса type

--- src/components/common/button/button.jsx
+++ src/components/common/button/button.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classes from './button.css'
 
-const Button = ({ children, typeButton, disabled, onClick }) => {
+const Button = ({ children, typeButton, disabled, onClick, type }) => {
   const classButton = [classes.button, classes[typeButton]]
-  console.log('typeButton', typeButton)
   return (
     <button
       onClick={onClick}
       className={classButton.join(' ')}
-      disabled={disabled}>
+      disabled={disabled}
+      type={type}>
       {children}
     </button>
   )
 }
 
 Button.propTypes = {
-  children: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
   typeButton: PropTypes.string,
+  type: PropTypes.string,
   disabled: PropTypes.bool,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func
 }
 export default Button
